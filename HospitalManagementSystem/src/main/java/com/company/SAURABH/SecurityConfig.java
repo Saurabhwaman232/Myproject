db3020f934ecf35d3157d3da2d72e2ffe9518900
Saurabhwaman232 <SAURABHWAMAN232@GMAIL.COM>
2023-04-15 11:07:40 +0530

    Hospital Management Syatem

---
+++ HospitalManagementSystem/src/main/java/com/company/SAURABH/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.company.SAURABH;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = 
+          PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+          .inMemoryAuthentication()
+          .withUser("SAURABH")
+          .password(encoder.encode("test123"))
+          .roles("USER","PATIENT")
+          .and()
+          .withUser("SANSKRITI")
+          .password(encoder.encode("test123"))
+          .roles("USER", "RECEPTIONIST")
+        .and()
+        .withUser("SAI")
+        .password(encoder.encode("test123"))
+        .roles("USER","DOCTOR")
+        .and()
+        .withUser("RAM")
+        .password(encoder.encode("test123"))
+        .roles("USER","DOCTOR")
+        .and()
+        .withUser("ABHI")
+        .password(encoder.encode("test123"))
+        .roles("USER","PATIENT")
+        .and()
+        .withUser("HARSH")
+        .password(encoder.encode("test123"))
+        .roles("USER","PATIENT")
+        .and()
+        .withUser("SIYA")
+        .password(encoder.encode("test123"))
+        .roles("USER","PATIENT")
+        .and()
+        .withUser("SAVI")
+        .password(encoder.encode("test123"))
+        .roles("USER","PATIENT");
+    }
+	
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+		 http
+         .csrf().disable()
+         .authorizeRequests()
+         .antMatchers("/").permitAll()
+         .antMatchers("/main").permitAll()
+         
+         .antMatchers("/signup").permitAll()
+         .antMatchers("/doctors/**").hasRole("DOCTOR")
+         .antMatchers("/patients/**").hasRole("PATIENT")
+         .antMatchers("/receptionist/**").hasRole("RECEPTIONIST")
+         .antMatchers("/anonymous*").anonymous()
+         .antMatchers("/login*").permitAll()
+         .anyRequest().authenticated()
+         .and()
+         .formLogin()
+   //     .loginPage("/login")
+//         .loginProcessingUrl("/perform_login")
+         .defaultSuccessUrl("/showPostLogin", false)
+         .permitAll()
+         //.failureUrl("/login.html?error=true")
+         //.failureHandler(authenticationFailureHandler())
+         .and()
+         .logout()
+         .logoutSuccessUrl("/")
+         .permitAll();
+	    }
+	
+}
---
+++ HospitalManagementSystem/src/main/java/com/company/SAURABH/invoiceRepository.java
@@ -0,0 +1,10 @@
+package com.company.SAURABH;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface invoiceRepository extends JpaRepository<invoice,Integer> {
+
+}
